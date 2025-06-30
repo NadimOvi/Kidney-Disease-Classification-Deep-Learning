f289cae3c3f511448d3d0fe922f6dc1787328033
NadimOvi <nadimovi20@gmail.com>
2025-06-30 03:27:25 +0200

    all commit

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "cnnClassifier"
+project_name = 'cnnClassifier'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
@@ -16,29 +16,33 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constant/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "resarch/trial.ipynb",
+    "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    
-    if filedir != "":
+
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
-    
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath, "w") as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
-        
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
         logging.info(f"{filename} is already exists")
